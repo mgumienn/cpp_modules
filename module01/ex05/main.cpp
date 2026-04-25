f72cd92c2daeb9a04000c753496dab53cceadcb0
Mateusz Gumienny <mateusznr7@gmail.com>
2026-04-25 19:32:59 +0200

    module01: ex05: added that harl can complain multiple times

--- module01/ex05/main.cpp
+++ module01/ex05/main.cpp
@@ -7,7 +7,13 @@ int main(void)
 	std::string level;
 	Harl harl;
 
-	std::cout << "Harl is just about to complain choose a level you want to hear (DEBUG / INFO / WARNING / ERROR)" << std::endl;
+	std::cout << "Harl is just about to complain! Choose a level you want to hear (DEBUG / INFO / WARNING / ERROR)" << std::endl;
 	std::getline(std::cin, level);
 	harl.complain(level);
+	while (true)
+	{
+		std::cout << "Harl is just about to complain AGAIN! Choose a level you want to hear (DEBUG / INFO / WARNING / ERROR)" << std::endl;
+		std::getline(std::cin, level);
+		harl.complain(level);
+	}
 }
