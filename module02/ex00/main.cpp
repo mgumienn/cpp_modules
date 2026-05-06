a7f86917e8ee51e6b11cb2b39174bbedf793a208
Mateusz Gumienny <mgumienn@c3r13s1.42warsaw.pl>
2026-05-06 17:48:45 +0200

    module02: ex00: went back to main from subject

--- module02/ex00/main.cpp
+++ module02/ex00/main.cpp
@@ -1,27 +1,9 @@
 #include <iostream>
 #include "Fixed.hpp"
 
-// int main( void ) {
-// 	Fixed a;
-// 	Fixed b( a );
-// 	Fixed c;
-
-// 	c = b;
-
-// 	std::cout << a.getRawBits() << std::endl;
-// 	std::cout << b.getRawBits() << std::endl;
-// 	std::cout << c.getRawBits() << std::endl;
-
-// 	return 0;
-// }
-
-int main(void)
-{
+int main( void ) {
 	Fixed a;
-
-	a.setRawBits(42);
-
-	Fixed b(a);
+	Fixed b( a );
 	Fixed c;
 
 	c = b;
@@ -29,5 +11,6 @@ int main(void)
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
-	return (0);
+
+	return 0;
 }
