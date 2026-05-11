7541146fe7863605b858958a76aabcaf78212eb4
Mateusz Gumienny <mgumienn@c4r1s4.42warsaw.pl>
2026-05-11 16:23:26 +0200

    module02: ex01: initial commit

---
+++ module02/ex01/Fixed.cpp
@@ -0,0 +1,73 @@
+#include "Fixed.hpp"
+#include <iostream>
+#include <cmath>
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->rawValue = 0;
+}
+
+Fixed::Fixed(const Fixed &fixed)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	this->rawValue = fixed.rawValue;
+}
+
+Fixed::Fixed(const int int_Value)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->rawValue = int_Value * (1 << this->bits);
+}
+
+Fixed::Fixed(const float float_Value)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->rawValue = roundf(float_Value * (1 << this->bits));
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &fixed)
+		this->rawValue = fixed.rawValue;
+	return *this;
+}
+
+std::ostream &operator<<(std::ostream& os, const Fixed& fixed)
+{
+	os << fixed.toFloat();
+	return os;
+}
+
+float Fixed::toFloat( void ) const
+{
+	float f_value;
+
+	f_value = this->rawValue / (1 << this->bits);
+	return (f_value);
+}
+
+int Fixed::toInt( void ) const
+{
+	return (this->rawValue >> 8);
+}
+
+
+int Fixed::getRawBits( void )
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->rawValue);
+}
+
+void Fixed::setRawBits( int const raw )
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->rawValue = raw;
+}
+
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
---
+++ module02/ex01/Fixed.hpp
@@ -0,0 +1,25 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+private:
+	int rawValue;
+	static const int bits = 8;
+public:
+	Fixed();
+	Fixed(const Fixed &fixed);
+	Fixed(const int int_Value);
+	Fixed(const float float_Value);
+	int getRawBits( void );
+	void setRawBits( int const raw );
+	float toFloat( void ) const;
+	int toInt( void ) const;
+	Fixed &operator=(const Fixed &fixed);
+	friend std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
+	~Fixed();
+};
+
+#endif
